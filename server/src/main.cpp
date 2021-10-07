8c638eaa1c976b0991c1dc469699dcc83d5cd397
Roman Astrakhantsev <rgastrakhantsev@gmail.com>
2021-10-07 05:00:35 +0300

    Simple write example with liburing

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -7,20 +7,44 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-#include "io_operation.hpp"
-#include "io_uring_driver.hpp"
+#include <liburing.h>
+
+#include <boost/log/trivial.hpp>
 
 
-using namespace hse;
+void uring_write(io_uring* ring,
+                 int fd,
+                 iovec data[],
+                 std::size_t data_size)
+{
+    struct io_uring_sqe sqe{};
+    struct io_uring_cqe* cqe;
+
+    sqe = *io_uring_get_sqe(ring);
+
+    io_uring_prep_writev(&sqe, fd, data, 1, 0);
+    io_uring_submit(ring);
+
+    io_uring_wait_cqe(ring, &cqe);
+
+    BOOST_LOG_TRIVIAL(debug) << "TOKEN=" << cqe->user_data << " done";
+    BOOST_LOG_TRIVIAL(debug) << "Opeation Result=" << cqe->res;
+    BOOST_LOG_TRIVIAL(debug) << "errno?=EBADF " << ((-cqe->res)==EBADF);
+    io_uring_cqe_seen(ring, cqe);
+
+}
+
 
 int main(){
-    int fd = open("test1.txt", O_RDWR | O_CREAT | O_TRUNC | O_NONBLOCK , 0666);
+
+    int fd = open("test1.txt", O_RDWR | O_CREAT | O_TRUNC | O_DIRECT , 0666);
     iovec iov[3];
 
     const char *buf[] = {
                     "The term buccaneer comes from the word boucan.\n",
                     "A boucan is a wooden frame used for cooking meat.\n",
-                    "Buccaneer is the West Indies name for a pirate.\n" };
+                    "Buccaneer is the West Indies name for a pirate.\n"
+    };
 
     for (int i = 0; i < 3; i++) {
             iov[i].iov_base = reinterpret_cast<std::byte*>(const_cast<char*>(buf[i]));
@@ -28,23 +52,11 @@ int main(){
     }
 
 
-    std::span<std::byte> buffer (reinterpret_cast<std::byte*>(iov), 3);
-    io_operation write_std =
-        {io_operation_type::write_vec,
-         buffer, fd, 0};
+    struct io_uring ring{};
+    io_uring_queue_init(4096, &ring, 0);
 
+    uring_write(&ring, fd, iov, 3);
+    uring_write(&ring, fd, iov, 3);
 
-    hse::io_uring_driver drv{4096};
-
-    auto token0 = drv.register_op();
-    auto token1 = drv.register_op();
-    auto token2 = drv.register_op(write_std);
-    auto token3 = drv.register_op();
-
-    drv.initiate(token1)
-            .wait(token1)
-            .initiate_all()
-            .wait_all();
-
-
+    io_uring_queue_exit(&ring);
 }
